79265ac10f0a37b016d6b19cdcb4abac0d5a26d4
menene <efmarroquinr@gmail.com>
2024-03-05 06:57:23 -0600

    tablas de multiplicar

--- Ejemplos/tablas_multiplicar.py
+++ Ejemplos/tablas_multiplicar.py
@@ -1,6 +1,5 @@
 x = int(input("Ingrese el número para ver su tabla de multiplicar: "))
-limite = int(input("Ingrese hasta donde quiere llegar: "))
 
-for i in range(limite + 1):
+for i in range(1, 11):
     resultado = (x * i)
     print(x, " x ", i, "=", resultado)
