c1deab80ab9cb24240523993c5f2a9be377ec4c8
menene <efmarroquinr@gmail.com>
2024-03-19 09:45:20 -0600

    Cadenas 1

---
+++ Ejemplos/Cadenas/cadenas1.py
@@ -0,0 +1,35 @@
+# c1 = "Hola"
+# c2 = "Mundo"
+# mensaje = c1 + " " + c2
+
+# print(mensaje)
+# print(c1)
+# print(c2)
+
+cadena = "HoLa MuNdO"
+largo = len(cadena)
+
+# print("El largo de la cadena es:", largo)
+# print("La cadena es alpha", cadena.isalpha())
+# print("La cadena es numerica", cadena.isdigit())
+# print("La cadena es flotante", cadena.isdecimal())
+# print(cadena.lower())
+# print(cadena.upper())
+# print(cadena.title())
+# print(cadena[0])
+# print(cadena[1])
+# # print(cadena[largo - 1])
+# print(cadena[-1])
+# print(cadena[-2])
+
+subcadena = cadena.lower()
+# print(subcadena.count("o"))
+
+# contador = 0
+# for letra in subcadena:
+#     if letra == "o":
+#         contador += 1
+
+#     print(letra)
+
+# print(contador)
